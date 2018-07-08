b9e4b047fe33b227e999f575b2de864a77af9b45
aelrady <ashley.elrady@gmail.com>
2018-07-08 18:17:06 -0400

    Update Ingredient.java

--- app/src/main/java/com/example/android/bakingapp/Model/Ingredient.java
+++ app/src/main/java/com/example/android/bakingapp/Model/Ingredient.java
@@ -1,6 +1,9 @@
 package com.example.android.bakingapp.Model;
 
-public class Ingredient {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Ingredient implements Parcelable {
 
     private Float quantity;
     private String measure;
@@ -49,4 +52,40 @@ public class Ingredient {
     public void setIngredient(String ingredient) {
         this.ingredient = ingredient;
     }
+
+    protected Ingredient(Parcel in) {
+        quantity = in.readByte() == 0x00 ? null : in.readFloat();
+        measure = in.readString();
+        ingredient = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (quantity == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeFloat(quantity);
+        }
+        dest.writeString(measure);
+        dest.writeString(ingredient);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
 }
