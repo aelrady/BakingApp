4e387f5959a40daccaa4496adc14b47d48fef045
aelrady <ashley.elrady@gmail.com>
2018-07-08 19:11:02 -0400

    Update StepDetailActivity.java

--- app/src/main/java/com/example/android/bakingapp/StepDetailActivity.java
+++ app/src/main/java/com/example/android/bakingapp/StepDetailActivity.java
@@ -1,13 +1,27 @@
 package com.example.android.bakingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.android.bakingapp.Model.Step;
 
 public class StepDetailActivity extends AppCompatActivity {
+
+    private Step step;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_detail);
+
+        Intent intent = getIntent();
+        step = intent.getParcelableExtra("step");
+        Log.v("Step: ", String.valueOf(step));
+
+
     }
 }
+
